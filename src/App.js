c548ea78b5cbff46bf7b0e2088a64eb1f747b63f
Ashutosh Prabhat <ashutoshsingh2645@gmail.com>
2022-07-18 23:18:50 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./MyComponents/Home";
+import Checkout from "./MyComponents/Checkout";
+import Login from "./MyComponents/Login";
+import Register from "./MyComponents/Register";
+import Payment from "./MyComponents/Payment";
+import Detail from "./MyComponents/Detail";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <>
+     
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="login" element={<Login />} />
+          <Route path="reg" element={<Register />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </>
+
+    </Router>
   );
 }
 
